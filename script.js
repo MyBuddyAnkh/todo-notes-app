ddd1183e91ef01ca7de00b3ce32eeb825223baa6
Anmol Khurmi <anmolkhurmi0909@gmail.com>
2026-05-25 17:22:47 -0400

    meess

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("note-form");
+const input = document.getElementById("note-input");
+const errorMessage = document.getElementById("error-message");
+const notesContainer = document.getElementById("notes-container");
+let notes = [];
